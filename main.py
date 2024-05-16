2992c36038e1fa5a50a5c28fb31990e62430b849
Anish Kumar Vedant <73393671+anishvedant@users.noreply.github.com>
2024-05-15 22:28:26 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import time
+from plyer import notification
+
+if __name__ == '__main__':
+
+	notification.notify(
+	title = "Book Padh lee saale!",
+	app_name="Reading",
+	ticker = "Hello",
+	message ="By creating a bedtime routine that includes reading, you can signal to your body that it is time to sleep.Therefore, by setting your phone aside and picking up a book, you are telling your brain that it is time to quiet down",
+	app_icon = "D:\\Python Code\\Notification remainder\\Paomedia-Small-N-Flat-Book-bookmark.ico",	
+	timeout= 20
+	)
+					
