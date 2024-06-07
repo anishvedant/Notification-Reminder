32361dbea9b64ef8d3abe4268462cbf0643d7385
Anish Kumar Vedant <73393671+anishvedant@users.noreply.github.com>
2024-06-06 23:47:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
 	app_name="Reading",
 	ticker = "Hello",
 	message ="By creating a bedtime routine that includes reading, you can signal to your body that it is time to sleep.Therefore, by setting your phone aside and picking up a book, you are telling your brain that it is time to quiet down",
-	app_icon = "D:\\Python Code\\Notification remainder\\Paomedia-Small-N-Flat-Book-bookmark.ico",	
+	app_icon = "Paomedia-Small-N-Flat-Book-bookmark.ico",	
 	timeout= 20
 	)
 					
