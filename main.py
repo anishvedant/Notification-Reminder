9e6eb53e326005ef53500205ccd60442e64b2ad2
Anish Kumar Vedant <73393671+anishvedant@users.noreply.github.com>
2024-06-17 18:06:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import time
 from plyer import notification
 
 if __name__ == '__main__':
-
-	notification.notify(
-	title = "Book Padh lee saale!",
-	app_name="Reading",
-	ticker = "Hello",
-	message ="By creating a bedtime routine that includes reading, you can signal to your body that it is time to sleep.Therefore, by setting your phone aside and picking up a book, you are telling your brain that it is time to quiet down",
-	app_icon = "Paomedia-Small-N-Flat-Book-bookmark.ico",	
-	timeout= 20
-	)
-					
+    # Call the notify method from the plyer library
+    notification.notify(
+        title = "Study Book",  # Title of the notification
+        app_name = "Reading",  # Name of the application sending the notification
+        ticker = "Hello",  # Ticker text for the notification
+        message = ("By creating a bedtime routine that includes reading, you can signal "
+                   "to your body that it is time to sleep. Therefore, by setting your phone "
+                   "aside and picking up a book, you are telling your brain that it is time to quiet down"),  # Notification message
+        app_icon = "Paomedia-Small-N-Flat-Book-bookmark.ico",  # Icon for the notification
+        timeout = 20  # Duration in seconds for the notification to stay visible
+    )
